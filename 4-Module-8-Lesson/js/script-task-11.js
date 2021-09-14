a8550d466876586b61067351c0af0f9e57a0b181
Yevhenii-bs <evgen.bs@gmail.com>
2021-09-14 12:24:51 +0300

    Update script-task-11.js

--- 4-Module-8-Lesson/js/script-task-11.js
+++ 4-Module-8-Lesson/js/script-task-11.js
@@ -22,3 +22,33 @@ result[1](); // 5, ожидаем 1
 result[2](); // 5, ожидаем 2
 result[3](); // 5, ожидаем 3
 result[4](); // 5, ожидаем 4
+
+
+// 4 module FUNCTION AS A PROPERTY  
+// Дополни код так, чтобы в переменной result был результат выполнения функции makePizza,
+// а в переменной pointer была ссылка на функцию makePizza
+
+// function makePizza() {
+//   return 'Ваша пицца готовится, ожидайте.';
+// }
+
+// const result = makePizza(); // результат выполнения функции makePizza
+// const pointer = makePizza; // ссылка на функцию makePizza
+
+// Дополни функцию makeMessage так, чтобы она ожидала вторым параметром (параметр callback) колбэк-функцию и возвращала ее вызов.
+// Функция deliverPizza или makePizza будет передаваться как колбэк и ожидать аргументом имя готовой доставляемой пиццы.
+
+function deliverPizza(pizzaName) {
+  return `Доставляем пиццу ${pizzaName}.`;
+}
+
+function makePizza(pizzaName) {
+  return `Пицца ${pizzaName} готовится, ожидайте...`;
+}
+
+// Пиши код ниже этой строки
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+}
+console.log(makeMessage);
+
